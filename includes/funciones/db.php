6897efdc563273f1bcb6c6896eba88f7dd62b882
ManuelBlaze <manuel.escobar1217@gmail.com>
2020-06-21 23:36:26 -0500

    Base de datos bd4free

--- includes/funciones/db.php
+++ includes/funciones/db.php
@@ -1,9 +1,9 @@
 <?php
 
     // credenciales base de datos
-    define('DB_USER', 'root');
-    define('DB_PASS', 'root');
-    define('DB_HOST', 'localhost');
+    define('DB_USER', 'manuelgithub');
+    define('DB_PASS', 'min8chars');
+    define('DB_HOST', '85.10.205.173:3306');
     define('DB_NAME', 'agendaphp');
 
     $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
